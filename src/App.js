145e4345ddf9eeddfde5113d7e6bb2bd4b879024
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-09-19 17:42:13 -0400

    Loading added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-
+  
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   const fetchMoviesHandler = async ()=>{
+    setIsLoading(true)
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
     const transformedMovies = data.results.map(movieData =>{
@@ -19,6 +21,7 @@ function App() {
       }
     })
       setMovies(transformedMovies);
+      setIsLoading(false)
   }
 
   return (
@@ -27,7 +30,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p> }
       </section>
     </React.Fragment>
   );
